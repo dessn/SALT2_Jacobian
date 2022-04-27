f939c8071fa4eb4a85be5f2dfbeec3da33a9d62e
Patrick Armstrong <u6096820@anu.edu.au>
2022-04-27 17:25:06 +1000

    Converted to OLUtils

--- src/SALT2_Jacobian.jl
+++ src/SALT2_Jacobian.jl
@@ -5,6 +5,7 @@ using TOML
 using LoggingExtras
 using PrettyTables
 using Statistics
+using OLUtils
 
 # Internal Packages
 include("Tools.jl")
@@ -20,82 +21,8 @@ using .Compare
 include("Plot.jl")
 using .Plot
 
-
 # Exports
 export process_jacobian
-
-function setup_global_config!(toml::Dict)
-    config = get(toml, "global", Dict())
-    # Base path is where everything relative will be relative to
-    # Defaults to the directory containing the toml path
-    # Can be relative (to the toml path) or absolute
-    base_path = get(config, "base_path", nothing)
-    if isnothing(base_path)
-        base_path = dirname(toml["toml_path"])
-    elseif !isabspath(base_path)
-        base_path = joinpath(dirname(toml["toml_path"]), base_path)
-    end
-    base_path = abspath(base_path)
-    config["base_path"] = base_path
-    # Output path is where all output (figures, files, etc...) will be placed
-    # Defaults to base_path / Output
-    # Can be relative (to base_path) or absolute
-    output_path = get(config, "output_path", nothing)
-    if isnothing(output_path)
-        output_path = joinpath(base_path, "Output")
-    elseif !isabspath(output_path)
-        output_path = joinpath(base_path, output_path)
-    end
-    config["output_path"] = abspath(output_path)
-    # Logging sets whether or not to setup and use Supernovae's logging
-    logging = get(config, "logging", true)
-    config["logging"] = logging
-    # Log file is the name of the log file. This will only work if logging is true
-    # Can only be relative to output_path
-    # Defaults to log.txt
-    log_file = get(config, "log_file", nothing)
-    if logging
-        if isnothing(log_file)
-            log_file = "log.txt"
-        end
-        log_file = abspath(joinpath(output_path, log_file))
-    end
-    if !logging & !isnothing(log_file)
-        @warn "Logging set to false, so log file $log_file will not be written. Please add `logger=true` to your [ global ] config"
-    end
-    config["log_file"] = log_file
-    toml["global"] = config
-end
-
-function setup_logger(log_file::AbstractString, verbose::Bool)
-    if verbose
-        level = Logging.Debug
-    else
-        level = Logging.Info
-    end
-    function fmt(io, args)
-        if args.level == Logging.Error
-            color = :red
-            bold = true
-        elseif args.level == Logging.Warn
-            color = :yellow
-            bold = true
-        elseif args.level == Logging.Info
-            color = :cyan
-            bold = false
-        else
-            color = :white
-            bold = false
-        end
-        printstyled(io, args._module, " | ", "[", args.level, "] ", args.message, "\n"; color = color, bold = bold)
-    end
-    logger = TeeLogger(
-        MinLevelLogger(FormatLogger(fmt, open(log_file, "w")), level),
-        MinLevelLogger(FormatLogger(fmt, stdout), level)
-    )
-    global_logger(logger)
-    @info "Logging to $log_file"
-end
 
 function jacobian_stage(toml, config)
     @info "Generating Jacobian"
@@ -243,21 +170,10 @@ function plot_stage(toml, config)
 end
 
 function process_jacobian(toml::Dict, batch_mode::Bool, verbose::Bool)
-    setup_global_config!(toml)
+    setup_global!(toml, verbose)
     config = toml["global"]
     num_trainopts = 0
     
-    # Ensure all path's exist
-    if !isdir(config["base_path"])
-        mkpath(config["base_path"])
-    end
-    if !isdir(config["output_path"])
-        mkpath(config["output_path"])
-    end
-    # Optionally set up logging
-    if config["logging"]
-        setup_logger(config["log_file"], verbose)
-    end
     @debug "Base path: $(config["base_path"])"
     @debug "Output path: $(config["output_path"])"
     @info "Running with $(Threads.nthreads()) threads"
@@ -293,7 +209,10 @@ end
 
 function process_jacobian(toml_path::AbstractString, batch_mode::Bool, verbose::Bool)
     toml = TOML.parsefile(toml_path)
-    toml["toml_path"] = abspath(toml_path)
+    if !("global" in keys(toml))
+        toml["global"] = Dict()
+    end
+    toml["global"]["toml_path"] = dirname(abspath(toml_path))
     return process_jacobian(toml, batch_mode, verbose)
 end
 
